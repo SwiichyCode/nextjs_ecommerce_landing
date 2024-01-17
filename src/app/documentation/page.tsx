71160aa296cdc4aa7ac57c784f4038783effa9e5
Swiichy <adlpromail@gmail.com>
2024-01-17 02:05:07 +0100

    first commit

---
+++ src/app/documentation/page.tsx
@@ -0,0 +1,5 @@
+import { Building } from "@/components/Building";
+
+export default function DocumentationPage() {
+  return <Building />;
+}
---
+++ src/constants/urls.ts
@@ -0,0 +1,2 @@
+export const DOCUMENTATION_URL = "/documentation";
+export const SHOP_URL = "https://ecommerce-template-three.vercel.app/shop";
